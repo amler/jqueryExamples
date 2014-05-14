602de80f873cfb861a7d700cdd1a6c586c98e2c0
Heather Amler <heatheramler@yahoo.com>
2014-05-13 22:37:27 -0400

    Added jQuery appendTo h2, .css transition, and start of fade in.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,43 @@
 //console.log('\'Allo \'Allo!');
-"use strict";
+'use strict';
 
-$(".one").click(function() {
-	if ( $( ".bluebox" ).is( ":hidden" ) ) {
-    	$( ".bluebox" ).show( "slow" );
-  	} else {
-    	$( ".bluebox" ).slideUp();
-  	}
+$('.one').click(function() {
+	if ( $( '.bluebox' ).is( ':hidden' ) ) {
+		$( '.bluebox' ).show( 'slow' );
+	} else {
+		$( '.bluebox' ).slideUp();
+	}
 });
 
+$('.two').click(function() {
+	$('<h2>Awesome Sauce!</h2>').appendTo('.textappend');
+
+});
+
+$('.three').click(function() {
+	$( '.cssmethod' ).css( {
+		'border-radius': '50%',
+		WebkitTransition : 'border-radius .2s ease-in-out',
+		MozTransition    : 'border-radius .2s ease-in-out',
+		transition       : 'border-radius .2s ease-in-out'
+	});
+});
+
+/*
+$("#kitten").hover(function(){
+
+    $(this).find("img").fadeOut();
+
+}, function() {
+
+    $(this).find("img").fadeIn();
+
+});
+*/
 
 //   var sureDelete = confirm("Are you sure you to delete this?");
 //   if (sureDelete === true) {
 /*      $('.bluebox').slideUp('slow', function() { 
-      	$('<p>Awesome Sauce!</p>').appendTo('.firstexample');
       });
    
 });
